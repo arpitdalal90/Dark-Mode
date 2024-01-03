a3dfd00acb26bdc610edb11ae4025e71e428d522
arpitdalal90 <139166398+arpitdalal90@users.noreply.github.com>
2024-01-03 18:23:13 +0530

    Delete Project-3 Dark Mode directory

--- Project-3 Dark Mode/script.js
+++
@@ -1,6 +0,0 @@
-const container = document.querySelector(".container");
-const toggle = document.querySelector(".darkModeButton"); 
-
-toggle.addEventListener("click", () => {
-    container.classList.toggle("dark") ? (toggle.firstElementChild.className = "far fa-moon") : (toggle.firstElementChild.className = "fa-solid fa-sun");
-});
